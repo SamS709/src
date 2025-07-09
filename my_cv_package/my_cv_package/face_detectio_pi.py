eb3f7570dbc346e93d698dd773d7c3cf573585c6
ubuntu <sami.leroux75@gmail.com>
2025-07-09 18:59:05 +0200

    coucou

--- my_cv_package/my_cv_package/face_detectio_pi.py
+++ my_cv_package/my_cv_package/face_detectio_pi.py
@@ -1,19 +1,40 @@
+#!/usr/bin/env/python3
+'''
+ SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+# Import the PCA9685 module. Available in the bundle and here:
+#   https://github.com/adafruit/Adafruit_CircuitPython_PCA9685
+# sudo pip3 install adafruit-circuitpython-motor
+# sudo pip3 install adafruit-circuitpython-pca9685
+'''
+import time
+from board import SCL, SDA
+import busio
+from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
 
+i2c = busio.I2C(SCL, SDA)
+# Create a simple PCA9685 class instance.
+pca = PCA9685(i2c, address=0x5f) #default 0x40
+
+pca.frequency = 50
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import numpy as np
 import cv2
-from ultralytics import YOLO
 import time
 
 
-model = YOLO("yolo11n.yaml")
+
 
 class ImageSubscriber(Node):
     def __init__(self):
         super().__init__('image_subscriber')
+        self.change = 5.0
+        self.servo_angle = servo.Servo(pca.channels[0], min_pulse=500, max_pulse=2400,actuation_range=180)
+        self.servo_angle.angle = 0
         self.subscription = self.create_subscription(
             Image,
             '/image_raw',
@@ -32,9 +53,15 @@ class ImageSubscriber(Node):
         faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
         W, H = vid.shape[1],vid.shape[0]
         for (x, y, w, h) in faces:
+            x0 = x + w/2
+            y0 = y + h/2
+            if y0> H/2 and self.servo_angle.angle<180.0 - self.change:
+                self.servo_angle.angle = self.servo_angle.angle + self.change
+            else :
+                if self.servo_angle.angle>self.change:
+                    self.servo_angle.angle = self.servo_angle.angle - self.change
             cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
-        cv2.imshow("Face Detection", vid)
-        cv2.waitKey(1)
+        
 
 def main(args=None):
     rclpy.init(args=args)
